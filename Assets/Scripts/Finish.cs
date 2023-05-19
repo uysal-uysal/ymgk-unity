7f24d5c44e3aefd864e55fbe146c6c2a42146445
uysal-uysal <uysal.meh0@gmail.com>
2023-05-19 16:56:43 +0300

    start-end screens

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private bool levelCompeled = false;
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !levelCompeled)
+        {
+            levelCompeled = true;
+            Invoke("CompleteLevel", 2f);
+        }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
